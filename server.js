f95a29628f4d6b315c21fc110a7990bf01576301
alvnavra <alvnavra@gmail.com>
2021-04-26 22:57:21 +0200

    Modificacion del server para que pueda interactuar con las respuestas de los usuarios

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const io = require('socket.io')(http,{
         origins:['172.104.152.232:8080']
     }
 })
+
+var answers = {}
+
 app.get('/', (req, res) => {
   res.send('<h1>Hey Socket.io</h1>');
 });
@@ -19,6 +22,49 @@ io.on('connection', (socket)=>{
     })
     socket.on('disconnect', ()=>{
         console.log('a user disconected')
+    })
+    socket.on('player_answer',(answer) => {
+      let resp = {id_question:answer.id_question, answer:answer.answwer}
+      console.log(answer)
+      console.log(answer.nickname)
+      console.log(answers)
+      if (answer.nickname in answers)
+      {
+        //Si el usuario existe, tenemos que ver si estamos
+        //insertando o modificando una respuesta.
+        
+        function devolver_idx(p_answer)
+        {
+          i = 0
+          console.table({'p_answer':p_answer})          
+          console.log(answers[answer.nickname].length)
+          console.log(answers[answer.nickname])
+          while(i < answers[answer.nickname].length)
+          {
+            if (answers[answer.nickname][i]['id_question']==p_answer.id_question) 
+            {
+              return i
+            }
+            i++
+          }
+          return -1
+          
+        }
+
+        let idx=devolver_idx(answer)        
+        if (idx >= 0) 
+        {
+            
+            answers[answer.nickname][idx] = resp
+        }
+        else answers[answer.nickname].push(resp)
+        console.log(answers)
+      }
+      else {
+        answers[answer.nickname] = [resp]
+        console.log(answers)
+      }
+      socket.emit('broadcast_answers',answers)
     })
 })
 
